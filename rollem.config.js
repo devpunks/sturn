67027ba686b9d8ab83594a7535c09182cac49028
Snuggs <rashaunstovall@gmail.com>
2017-10-08 18:51:23 -0400

    Add rollem configuration

---
+++ rollem.config.js
@@ -0,0 +1,11 @@
+// for components
+
+module.exports = [
+  {
+    entry: 'media/script.es',
+    dest: 'public/media.js'
+  },
+  {
+    entry: 'preferences/script.es',
+    dest: 'public/preferences.js'
+}]
